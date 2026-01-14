e2dc8db7e8a93b2f9ffe5e3710e1f7714bfa24bc
Nightski <kevinvinsent205@gmail.com>
2026-01-14 13:57:48 +0530

    maza

---
+++ first.js
@@ -0,0 +1,18 @@
+//variable
+let x = 5;
+let y = 10;
+console.log(x+y);
+const name = "Kevin";
+
+//conditions
+let p = 19
+if(p % 2 == 0){
+    console.log("Even");
+}
+else{
+    console.log("Odd");
+}
+
+let q = 10;
+q % 2 == 0 ? console.log("Even") : console.log("Odd");
+
