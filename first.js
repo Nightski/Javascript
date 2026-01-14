12d555a104a9dcc3a4d1da4aa5552f1ffc486582
Nightski <kevinvinsent205@gmail.com>
2026-01-14 14:23:43 +0530

    final practice

--- first.js
+++ first.js
@@ -1,23 +1,22 @@
 //variable
 let x = 5;
 let y = 10;
-console.log(x+y);
 const name = "Kevin";
 
 //conditions
 let p = 19
 if(p % 2 == 0){
-    console.log("Even");
+    //console.log("Even");
 }
 else{
-    console.log("Odd");
+    //console.log("Odd");
 }
 
 let q = 10;
-q % 2 == 0 ? console.log("Even") : console.log("Odd");
+//q % 2 == 0 ? console.log("Even") : console.log("Odd");
 
 //looping
 let car = ["porsche", "BMW", "Jaguar"]
 for (let i = 0;i<car.length;i++){
-    console.log(car[i])
+    //console.log(car[i])
 }
